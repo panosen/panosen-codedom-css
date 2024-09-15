761a77b53d77c95aa0e4d09518701c4bc3dbda70
harriszhang <harriszhang@live.cn>
2024-09-15 19:31:54 +0800

    1.3.1

--- 0 - Build-Publish-Move.bat
+++ 0 - Build-Publish-Move.bat
@@ -4,6 +4,9 @@ dotnet restore
 
 dotnet build --no-restore -c Release
 
+dotnet nuget push Panosen.CodeDom.Css\bin\Release\Panosen.CodeDom.Css.*.nupkg -s https://nuget.panosen.cn/v3/index.json -k 1cd8e026-9715-3c58-aa2c-42cd087c0b88 --skip-duplicate
+dotnet nuget push Panosen.CodeDom.Css.Engine\bin\Release\Panosen.CodeDom.Css.Engine.*.nupkg -s https://nuget.panosen.cn/v3/index.json -k 1cd8e026-9715-3c58-aa2c-42cd087c0b88 --skip-duplicate
+
 move /Y Panosen.CodeDom.Css\bin\Release\Panosen.CodeDom.Css.*.nupkg D:\LocalSavoryNuget\
 move /Y Panosen.CodeDom.Css.Engine\bin\Release\Panosen.CodeDom.Css.Engine.*.nupkg D:\LocalSavoryNuget\
 
